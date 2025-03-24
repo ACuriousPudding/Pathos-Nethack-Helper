9d1179c202a1aabdf2802b832e274eb6a1a01111
Cheshire <cheshirediore@gmail.com>
2025-03-24 19:00:28 -0400

    Add comments to RotationQuestion.cs

--- Pathos-Nethack-Helper/LostChambers/RotationQuestion.cs
+++ Pathos-Nethack-Helper/LostChambers/RotationQuestion.cs
@@ -2,8 +2,25 @@
 
 namespace Pathos_Nethack_Helper.LostChambers
 {
+    /**
+     * A question that can be asked to determine the orientation of a room. The index of the answer value
+     * indicates the number of 90 degree rotations clockwise from the original map, with 0 being the original
+     * map, 1 being 90 degrees clockwise, 2 being 180 degrees, and 3 being 270 degrees clockwise.
+     */
     public class RotationQuestion
     {
+
+        /**
+         * Constructor for the RotationQuestion class.
+         * <p>
+         * The answers must be listed in ascending order of rotation distance. The first answer will
+         * be 0 degrees, the second will be 90 degrees, etc.
+         * <p>
+         * @param roomForQuestion The room for which the question applies.
+         * @param questionText The text of the question. This is the text that is displayed to the user.
+         * @param answers A string array of the possible answers to the question. Array must be of length 4.
+         * @return A RotationQuestion object.
+         */
         public RotationQuestion(LostChamber.RoomName roomForQuestion, string questionText, string[] answers)
         {
             RoomForQuestion = roomForQuestion;
@@ -11,9 +28,11 @@ namespace Pathos_Nethack_Helper.LostChambers
             Answers = answers;
         }
 
+        // TODO: Add validation to check if room is a valid LostChamber.RoomName value
         public LostChamber.RoomName RoomForQuestion { get; set; }
         public string QuestionText { get; set; }
         
+        // TODO: Add validation to the setter to ensure the array length is 4.
         /** Any answers must be listed in ascending order of rotation distance. The first answer will
          * be 0 degrees, second will be 90 degrees, etc.
          */
